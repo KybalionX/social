022a6a315f5b9028a36bdf26fb8b090b784242f3
Nicolas <=>
2021-12-09 11:31:57 -0500

    Agregados modulos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const app = express();
+const colors = require('colors');
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
 console.log("INITIALIZED");
