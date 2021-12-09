bb11db98ae342e85f4a0951b6be213502d500742
Nicolas <=>
2021-12-09 14:05:32 -0500

    Added dependencies

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const colors = require('colors');
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', (req, res) => {
     res.send("Hello world");
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Server running in port ${app.get('port')}`.rainbow);
 });
 
 console.log("INITIALIZED");
